d953dc39bd11ac08f3eb225732c1ccc3f156089e
Tianyi Hu <hutianyiyi@yahoo.cn>
2012-03-16 13:54:32 -0600

    This file just include the header file and run the test function Run().

---
+++ Implementation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <stdexcept>
+using std::runtime_error ;
+#include <typeinfo>
+#include <cmath>
+#include<iomanip>
+using namespace std;
+
+#include "PROJECTHEADER.h"
+#include "Derivative.h"
+
+
+
+
+int main()
+{	
+	Run();
+	return 0;
+}
+
+
